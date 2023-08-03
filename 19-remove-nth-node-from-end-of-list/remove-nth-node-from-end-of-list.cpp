b6d8f30a72e4127da6d8b589787d88acfa9cf4cf
PRANJAL <95075442+pranjalrana01@users.noreply.github.com>
2023-08-04 00:41:46 +0530

    Time: 4 ms (52.81%) | Memory: 10.7 MB (38.88%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -8,23 +8,56 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* removeNthFromEnd(ListNode* head, int n) {
+//         ListNode* start = new ListNode();
+//         start -> next = head;
+//         ListNode* slow = start;
+//         ListNode* fast = start;
+
+//         for(int i=1 ; i<=n ; i++){
+//             fast = fast -> next;
+//         }
+//         while(fast -> next != NULL){
+//             fast = fast -> next;
+//             slow = slow -> next;
+//         }
+
+//         slow -> next = slow -> next -> next;
+//         return start -> next;
+//     }
+// };
+
+
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* start = new ListNode();
-        start -> next = head;
-        ListNode* slow = start;
-        ListNode* fast = start;
 
-        for(int i=1 ; i<=n ; i++){
-            fast = fast -> next;
+        ListNode* ptr = head;
+        int counter =0;
+        while(ptr!=NULL)
+        {
+            counter++;
+            ptr = ptr->next;
         }
-        while(fast -> next != NULL){
-            fast = fast -> next;
-            slow = slow -> next;
+        if(head ==NULL || head->next == NULL)
+        {
+            return NULL;
         }
-
-        slow -> next = slow -> next -> next;
-        return start -> next;
+        if(counter - n == 0)
+         {
+             head = head->next;
+             return head;
+         }
+        ListNode* temp=head;
+        ListNode* slow=head;
+        for(int i=0;i<counter-n;i++)
+        {
+            slow = temp;
+            temp = temp->next;
+        }
+        slow->next = temp->next;
+        return head;  
     }
 };
