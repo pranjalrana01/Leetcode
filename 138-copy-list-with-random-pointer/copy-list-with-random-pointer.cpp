98ff4d0e4dee8b79e326b8865805a0a1bcae3811
PRANJAL <95075442+pranjalrana01@users.noreply.github.com>
2023-08-03 09:38:35 +0530

    Time: 3 ms (97.82%) | Memory: 11.3 MB (57.81%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -29,42 +29,90 @@ public:
             tail = newNode;
         }
     }
-    Node* copyRandomList(Node* head) {
-         Node* cloneHead = NULL;
+    // Node* copyRandomList(Node* head) {
+    //      Node* cloneHead = NULL;
+    //     Node* cloneTail = NULL;
+        
+    //     Node* temp = head;
+        
+    //     while (temp !=NULL){
+    //         insertAtTail(cloneHead , cloneTail , temp -> val);
+    //         temp = temp -> next;
+    //     }
+        
+    //     //step2 create a map to store original node and clone node linking
+        
+    //     unordered_map<Node* , Node*>oldToNewNode;
+    //     Node* originalNode = head;
+    //     Node* cloneNode = cloneHead;
+        
+    //     while(originalNode != NULL){
+    //         oldToNewNode[originalNode] = cloneNode;
+    //         originalNode = originalNode -> next;
+    //         cloneNode = cloneNode -> next;
+    //     }
+        
+    //     originalNode = head;
+    //     cloneNode = cloneHead;
+        
+    //     while(originalNode != NULL){
+    //         cloneNode -> random = oldToNewNode[originalNode -> random];
+    //         cloneNode = cloneNode -> next;
+    //         originalNode = originalNode -> next;
+    //     }
+        
+    //     return cloneHead;
+        
+        
+    
+    Node* copyRandomList(Node* head){
+        
+        Node* cloneHead = NULL;
         Node* cloneTail = NULL;
-        
+
         Node* temp = head;
-        
-        while (temp !=NULL){
+
+       while(temp != NULL){
             insertAtTail(cloneHead , cloneTail , temp -> val);
             temp = temp -> next;
-        }
-        
-        //step2 create a map to store original node and clone node linking
-        
-        unordered_map<Node* , Node*>oldToNewNode;
-        Node* originalNode = head;
-        Node* cloneNode = cloneHead;
-        
-        while(originalNode != NULL){
-            oldToNewNode[originalNode] = cloneNode;
-            originalNode = originalNode -> next;
+       }
+
+       //step2 change pointers of the linked list
+
+       Node* originalNode = head;
+       Node* cloneNode = cloneHead;
+
+       while(originalNode != NULL && cloneNode != NULL){
+          Node* next = originalNode -> next;
+          originalNode -> next = cloneNode;
+          originalNode = next;
+
+          next = cloneNode -> next;
+          cloneNode -> next = originalNode;
+          cloneNode = next;
+       }
+
+         temp = head;
+       while(temp != NULL){
+           if(temp -> next != NULL){
+               temp -> next -> random = temp -> random ? temp -> random -> next : temp -> random;
+           }
+           temp = temp -> next -> next;
+       }
+           
+      originalNode = head;
+      cloneNode = cloneHead;
+
+       while(originalNode != NULL && cloneNode != NULL){
+           originalNode -> next = cloneNode -> next;
+           originalNode = originalNode -> next;
+           if(originalNode !=NULL){
+           cloneNode -> next = originalNode -> next;
+            }
             cloneNode = cloneNode -> next;
-        }
-        
-        originalNode = head;
-        cloneNode = cloneHead;
-        
-        while(originalNode != NULL){
-            cloneNode -> random = oldToNewNode[originalNode -> random];
-            cloneNode = cloneNode -> next;
-            originalNode = originalNode -> next;
-        }
-        
-        return cloneHead;
-        
-        
-        
-        
+       }
+       return cloneHead;
+
+
     }
 };
