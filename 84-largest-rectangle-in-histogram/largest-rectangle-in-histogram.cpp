a8164fda1198d64815325cf463981ceb166ac85f
PRANJAL <95075442+pranjalrana01@users.noreply.github.com>
2023-08-10 11:22:19 +0530

    Time: 197 ms (62.50%) | Memory: 98.2 MB (7.90%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,67 +1,85 @@
-class Solution {
-public:
-    vector<int> NSL(vector<int>& heights) {
-        vector<int> left;
+ class Solution {
+ public:
+
+ //solution 1
+
+    vector<int>NSL(vector<int>& heights){
+        vector<int>left;
         int n = heights.size();
         int pseudoindex = -1;
-        stack<pair<int,int>> stk;
-        for (int i = 0; i < n; i++) {
-            while (!stk.empty() && stk.top().first >= heights[i]) {
-                stk.pop();
-            }
-            if (stk.empty()) {
+        stack<pair<int,int>>stk;
+        for(int i=0 ; i<n ; i++){
+            if(stk.empty()){
                 left.push_back(pseudoindex);
-            } else {
+            }
+            else if(stk.size() > 0 && stk.top().first < heights[i]){
                 left.push_back(stk.top().second);
             }
-            stk.push({heights[i], i});
+            else if(stk.size() > 0 && stk.top().first >= heights[i]){
+                while(stk.size() > 0 && stk.top().first >= heights[i]){
+                    stk.pop();
+                }
+                if(stk.size() == 0)left.push_back(pseudoindex);
+                else{
+                    left.push_back(stk.top().second);
+                }
+            }
+            stk.push({heights[i] , i});
         }
         return left;
     }
 
-    vector<int> NSR(vector<int>& heights) {
-        vector<int> right;
+       vector<int>NSR(vector<int>& heights){
+           vector<int>right;
+           int n  = heights.size();
+           stack<pair<int,int>>s;
+           int pseudoindex = n;
+
+           for(int i= n-1 ; i>=0 ; i--){
+               if(s.size() == 0){
+                   right.push_back(pseudoindex);
+               }
+               else if(s.size() > 0 && s.top().first < heights[i]){
+                   right.push_back(s.top().second);
+               }
+                else if(s.size() > 0 && s.top().first >= heights[i]){
+                    while(s.size() > 0 && s.top().first >= heights[i]){
+                        s.pop();
+                    }
+                    if(s.size() == 0){
+                        right.push_back(pseudoindex);
+                    }
+                    else{
+                        right.push_back(s.top().second);
+                    }
+                }
+                s.push({heights[i], i});
+           }
+           reverse(right.begin(), right.end());
+           return right;
+       }
+
+  int largestRectangleArea(vector<int>& heights) {
+
+      //solution 1
         int n = heights.size();
-        int pseudoindex = n;
-
-        stack<pair<int,int>> s;
-
-        for (int i = n - 1; i >= 0; i--) {
-            while (!s.empty() && s.top().first >= heights[i]) {
-                s.pop();
-            }
-            if (s.empty()) {
-                right.push_back(pseudoindex);
-            } else {
-                right.push_back(s.top().second);
-            }
-            s.push({heights[i], i});
-        }
-        reverse(right.begin(), right.end()); // Correct the order
-        return right;
-    }
-
-    int largestRectangleArea(vector<int>& heights) {
-        int n = heights.size();
-        vector<int> left = NSL(heights);
-        vector<int> right = NSR(heights);
-        vector<int> width(n);
-        vector<int> area(n);
-
-        for (int i = 0; i < n; i++) {
+        vector<int>left = NSL(heights);
+        vector<int>right = NSR(heights);
+        vector<int>width(n);
+        vector<int>area(n);
+        for(int i=0 ; i<n ; i++){
             width[i] = right[i] - left[i] - 1;
         }
 
-        for (int i = 0; i < n; i++) {
+        for(int i=0; i <n ; i++){
             area[i] = width[i] * heights[i];
         }
-
-        int maxi = 0; // Initialize to 0 instead of -23
-        for (int i = 0; i < n; i++) {
-            if (area[i] > maxi) {
+        int maxi = -23;
+        for(int i = 0;i <n ; i++){
+            if(area[i] > maxi){
                 maxi = area[i];
             }
         }
         return maxi;
     }
-};
+ };
