cc09364af29060ac52ff41093d71e82536c24ae8
PRANJAL <pranjalrana474@gmail.com>
2023-08-17 21:18:26 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (41.40%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,34 +1,3 @@
-// class Solution {
-// public:
-//     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-//         if(n==0)
-//         return;
-//         if(m==0){
-//             nums1=nums2;
-//             return;
-//         }
-        
-//         int i=m-1;
-//         int k=nums1.size()-1;
-//         int j=n-1;
-//         while(i>=0&&j>=0){
-//             if(nums1[i]>nums2[j]){
-//                 nums1[k--]=nums1[i--];
-//             }
-//             else if(nums1[i]<=nums2[j]){
-//                 nums1[k--]=nums2[j--];
-//             }
-//         }
-//         while(j>=0){
-//             nums1[j]=nums2[j];
-//             j--;
-//         }
-        
-//     }
-// };
-
-
-
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
@@ -43,9 +12,9 @@ public:
                     j-- ;
                 }
         }
-        while(j>=0){
-                    nums1[j]=nums2[j];
-                    j-- ;
-                }      
+        while (j >= 0) {
+            nums1[i + j + 1] = nums2[j];
+            j--;
+}
     }
 };
