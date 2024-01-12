888ee2437c36b0849e104aaa913750beac0444d0
PRANJAL <pranjalrana474@gmail.com>
2024-01-12 22:11:02 +0530

    Time: 0 ms (100.00%) | Memory: 7 MB (72.68%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.cpp
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
-    bool halvesAreAlike(string s) {
-            auto countVowels = [](const string& str) {
-            unordered_set<char> vowels{'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
-            int count = 0;
-            for (char c : str) {
-                if (vowels.count(c) > 0) {
-                    count++;
-                }
-            }
-            return count;
-        };
+    bool halvesAreAlike(std::string s) {
+        unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        int vowelsCount = 0;
+        int midIndex = s.length() / 2;
 
-        int length = s.length();
-        int midPoint = length / 2;
+        for (int i = 0; i < midIndex; i++) {
+            char charA = s[i];
+            char charB = s[midIndex + i];
+            if (vowels.count(charA)) vowelsCount++;
+            if (vowels.count(charB)) vowelsCount--;
+        }
 
-        string firstHalf = s.substr(0, midPoint);
-        string secondHalf = s.substr(midPoint);
-
-        return countVowels(firstHalf) == countVowels(secondHalf);
+        return vowelsCount == 0;
     }
 };
