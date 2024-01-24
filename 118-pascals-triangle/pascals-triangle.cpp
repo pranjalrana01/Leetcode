4ebd29536ae3a12dab8363990c5b7934fcd68e86
PRANJAL <pranjalrana474@gmail.com>
2024-01-24 22:36:45 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (11.31%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,38 +1,16 @@
 class Solution {
 public:
-    // vector<int>generateRow(int row){
-    //     long long ans = 1;
-    //     vector<int>ansRow;
-    //     ansRow.push_back(1);
-    //     for(int col=1 ; col < row ; col++){
-    //         ans = ans * (row-col);
-    //         ans = ans/(col);
-    //         ansRow.push_back(ans);
-    //     }
-    //     return ansRow;
-    // }
-    // vector<vector<int>> generate(int numRows) {
-    //     vector<vector<int>>ans;
-    //     for(int i=1 ; i<=numRows ; i++){
-    //         ans.push_back(generateRow(i));
-    //     }
-    //     return ans;
-    //   }
+    vector<vector<int>> generate(int numRows){
+        vector<vector<int>>result(numRows);
 
+        for(int i=0 ; i<numRows ; i++){
+            result[i].resize(i+1);
+            result[i][0] = result[i][i] = 1;
 
-        //solution 2
-
-        vector<vector<int>> generate(int numRows){
-            vector<vector<int>>r(numRows);
-
-            for(int i=0 ; i<numRows ; i++){
-                r[i].resize(i+1);
-                r[i][0] = r[i][i] = 1;
-
-                for(int j=1 ; j<i ; j++){
-                    r[i][j] = r[i-1][j-1] + r[i-1][j];
-                }
+            for(int j=1 ; j<i ; j++){
+                result[i][j] = result[i-1][j-1] + result[i-1][j];
             }
-                return r;
-            }
+        }
+        return result;
+    }
 };
