17e2ea611dcd86037114084c30f07b76074b56a2
PRANJAL <pranjalrana474@gmail.com>
2024-01-31 19:03:05 +0530

    Time: 120 ms (90.50%) | Memory: 101.1 MB (64.78%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -2,18 +2,42 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-       int n = temperatures.size();
-        vector<int> ans;
-        stack<int> st;
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty() && temperatures[i]>=temperatures[st.top()]){
-                st.pop();
+    //    int n = temperatures.size();
+    //     vector<int> ans;
+    //     stack<int> st;
+    //     for(int i=n-1;i>=0;i--){
+    //         while(!st.empty() && temperatures[i]>=temperatures[st.top()]){
+    //             st.pop();
+    //         }
+    //         int nd = st.empty() ? 0 : st.top()-i;
+    //         ans.push_back(nd);
+    //         st.push(i);
+    //     }
+    //     reverse(ans.begin(),ans.end());
+    //     return ans;
+
+
+    int n = temperatures.size();
+    vector<int>ans;
+    stack<pair<int,int>>st;
+
+        for(int i= n-1 ; i>=0 ; i--){
+            int val = 1;
+            int element = temperatures[i];
+
+            if(st.empty()){
+                temperatures[i] = 0;
             }
-            int nd = st.empty() ? 0 : st.top()-i;
-            ans.push_back(nd);
-            st.push(i);
+            else{
+                while(!st.empty() && st.top().first <= temperatures[i]){
+                    val += st.top().second;
+                    st.pop();
+                }
+                (st.empty()) ? temperatures[i] = 0 : temperatures[i] = val;
+            }
+            st.push({element , val});
         }
-        reverse(ans.begin(),ans.end());
-        return ans;
+        return temperatures;
     }
+    
 };
