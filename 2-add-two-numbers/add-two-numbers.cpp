577c3211e1a97f992233d15977704d1e9d952119
PRANJAL <pranjalrana474@gmail.com>
2024-03-15 23:38:03 +0530

    Time: 29 ms (18.09%) | Memory: 76.1 MB (15.33%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -9,12 +9,12 @@
  * };
  */
 class Solution {
-
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-         ListNode* dummy=new ListNode();
+        ListNode* dummy=new ListNode();
         ListNode* temp=dummy;
         int carry=0;
+
         while(l1!=NULL || l2!=NULL || carry){
             int sum=0;
             if(l1!=NULL){
