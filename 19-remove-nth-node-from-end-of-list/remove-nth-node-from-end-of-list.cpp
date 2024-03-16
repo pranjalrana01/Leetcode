f6ec6dad7294a57c502b0695d39d4ae5a413ef21
PRANJAL <pranjalrana474@gmail.com>
2024-03-16 22:44:24 +0530

    Time: 4 ms (34.40%) | Memory: 13.2 MB (59.80%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -8,56 +8,23 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-// class Solution {
-// public:
-//     ListNode* removeNthFromEnd(ListNode* head, int n) {
-//         ListNode* start = new ListNode();
-//         start -> next = head;
-//         ListNode* slow = start;
-//         ListNode* fast = start;
-
-//         for(int i=1 ; i<=n ; i++){
-//             fast = fast -> next;
-//         }
-//         while(fast -> next != NULL){
-//             fast = fast -> next;
-//             slow = slow -> next;
-//         }
-
-//         slow -> next = slow -> next -> next;
-//         return start -> next;
-//     }
-// };
-
-
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* start = new ListNode();
+        start -> next = head;
+        ListNode* slow = start;
+        ListNode* fast = start;
 
-        ListNode* ptr = head;
-        int counter =0;
-        while(ptr!=NULL)
-        {
-            counter++;
-            ptr = ptr->next;
+        for(int i=1 ; i<=n ; i++){
+            fast = fast -> next;
         }
-        if(head ==NULL || head->next == NULL)
-        {
-            return NULL;
+         while(fast -> next != NULL){
+            fast = fast -> next;
+            slow = slow -> next;
         }
-        if(counter - n == 0)
-         {
-             head = head->next;
-             return head;
-         }
-        ListNode* temp=head;
-        ListNode* slow=head;
-        for(int i=0;i<counter-n;i++)
-        {
-            slow = temp;
-            temp = temp->next;
-        }
-        slow->next = temp->next;
-        return head;  
+
+        slow -> next = slow -> next -> next;
+        return start -> next;
     }
 };
