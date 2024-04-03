ede64d4d683a6c9ae8d8b4add6668e847da1c8c2
PRANJAL <pranjalrana474@gmail.com>
2024-04-03 22:45:02 +0530

    Time: 195 ms (90.81%) | Memory: 9.6 MB (78.94%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    bool search(vector<vector<char>>& board, const string& word, int index, int row, int col){
-        if(index == word.size()) return true;
+    bool search(vector<vector<char>>& board, const string& word, int index,int row, int col){
+        if(index == word.size())return true;
 
         int rows = board.size();
         int cols = board[0].size();
 
-        if(row<0 || col<0 || row >= rows || col >= cols || board[row][col] != word[index]) return false;
+        if(row<0 || col<0 || row>=rows || col>=cols || board[row][col] != word[index])return false;
 
         char temp = board[row][col];
         board[row][col] = '.';
@@ -18,17 +18,17 @@ public:
 
         board[row][col] = temp;
         return found;
+
     }
 
     bool exist(vector<vector<char>>& board, string word) {
         if(board.empty() || board[0].empty() || word.empty()) return false;
-
         int rows = board.size();
         int cols = board[0].size();
 
-        for(int i=0 ; i<rows ; ++i){
-            for (int j=0 ; j<cols ; ++j){
-                if(search(board, word, 0, i, j)) return true;
+        for(int i=0; i<rows ; i++){
+            for(int j=0 ; j<cols ; j++){
+                if(search(board, word, 0, i, j))return true;
             }
         }
         return false;
