33033b77c08a2d70b9db1ffbd783370e7eb46351
PRANJAL <pranjalrana474@gmail.com>
2024-04-04 21:42:20 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (62.31%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
-
-    void solve(string digit, string output, vector<string>&ans, int index, string mapping[]){
+    void solve(string digit, string output,int index, string mapping[], vector<string>& ans){
         if(index >= digit.length()){
             ans.push_back(output);
             return;
         }
 
-        int number = digit[index]-'0';
+        int number = digit[index] - '0';
         string value = mapping[number];
 
-        for(int i=0 ; i<value.length() ; i++){
+        for(int i=0 ; i<value.length(); i++){
             output.push_back(value[i]);
-            solve(digit, output, ans, index+1, mapping);
+            solve(digit, output, index+1, mapping, ans);
+
             output.pop_back();
         }
+
     }
+
     vector<string> letterCombinations(string digits) {
         vector<string>ans;
         if(digits.size() == 0){
             return ans;
         }
-
-        string output="";
+        string output = "";
         int index = 0;
 
         string mapping[10] = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        
-        solve(digits,output,ans,index, mapping);
+
+        solve(digits,output, index, mapping, ans);
         return ans;
     }
 };
