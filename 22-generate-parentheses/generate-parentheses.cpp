be0b0994b73fa05ef77294fdb9965530cd2ba617
PRANJAL <pranjalrana474@gmail.com>
2024-04-04 22:36:59 +0530

    Time: 3 ms (60.58%) | Memory: 15.5 MB (31.17%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,40 +1,59 @@
+// class Solution {
+// public:
+    
+//     void solve(vector<string>& ans, string op, int open, int close){
+//         if(open == 0 && close == 0){
+//             ans.push_back(op);
+//             return;
+//         }
+
+//         if(open == close || close == 0){
+//             string op1 = op;
+//             op1.push_back('(');
+//             solve(ans, op1, open-1, close);
+//         }
+
+//         else if(open == 0){
+//             string op1 = op;
+//             op1.push_back(')');
+//             solve(ans, op1, open, close-1);
+//         }
+
+//         else{
+//             string op1 = op;
+//             string op2 = op;
+//             op1.push_back('(');
+//             op2.push_back(')');
+//             solve(ans, op1, open-1, close);
+//             solve(ans, op2, open, close-1);
+//         }
+//     }
+    
+//     vector<string> generateParenthesis(int n) {
+//         int open = n;
+//         int close = n;
+//         string op="";
+//         vector<string>ans;
+//         solve(ans, op, open, close);
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
-    
-    void solve(vector<string>& ans, string op, int open, int close){
-        if(open == 0 && close == 0){
-            ans.push_back(op);
+    vector<string>v;
+    void solve(int i , int j , int n , string s) {
+        if(i == j and i == n) {
+            v.push_back(s);
             return;
         }
+        if(i < n) solve(i + 1 , j , n , s + '(');
+        if(j < i) solve(i  , j + 1 , n , s + ')');
 
-        if(open == close || close == 0){
-            string op1 = op;
-            op1.push_back('(');
-            solve(ans, op1, open-1, close);
-        }
-
-        else if(open == 0){
-            string op1 = op;
-            op1.push_back(')');
-            solve(ans, op1, open, close-1);
-        }
-
-        else{
-            string op1 = op;
-            string op2 = op;
-            op1.push_back('(');
-            op2.push_back(')');
-            solve(ans, op1, open-1, close);
-            solve(ans, op2, open, close-1);
-        }
     }
-    
     vector<string> generateParenthesis(int n) {
-        int open = n;
-        int close = n;
-        string op="";
-        vector<string>ans;
-        solve(ans, op, open, close);
-        return ans;
+        v.clear();
+        solve(0, 0 , n , "");
+        return v;
     }
 };
