4fa14fea4db391d43dc7a2870fea0b83d38b1f54
PRANJAL <pranjalrana474@gmail.com>
2024-04-19 10:27:05 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (82.50%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
@@ -4,21 +4,17 @@ public:
          if (head == NULL || head->next == NULL) {
             return head;
         }
-
         ListNode* node = new ListNode(-1);
         ListNode* curr = head,* prev = node;
 
         while(curr != NULL && curr -> next != NULL){
-            prev->next = curr -> next;
+            prev -> next = curr -> next;
             curr -> next = curr -> next -> next;
-            prev -> next -> next = curr;
 
+            prev -> next -> next = curr;
             curr = curr -> next;
             prev = prev -> next -> next;
-
         }
-
         return node -> next;
-
     }
 };
