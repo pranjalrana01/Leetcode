5e73fc4b6e1f44cbb86af8f512c7c80b8688f4ed
PRANJAL <pranjalrana474@gmail.com>
2024-05-22 11:55:21 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (99.56%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     int numDecodings(std::string s) {
-        if (s.empty() || s[0] == '0') {
-            return 0;
+        if (s.empty() || s[0] == '0') return 0;
+    
+    int a = 1, b = 1;  // a: dp[i-2], b: dp[i-1]
+    
+    for (size_t i = 1; i < s.size(); ++i) {
+        int temp = b;
+        if (s[i] == '0') {
+            if (s[i - 1] > '2' || s[i - 1] == '0') return 0;
+            b = a;
+        } else if (s[i - 1] == '1' || (s[i - 1] == '2' && s[i] < '7')) {
+            b = b + a;
         }
+        a = temp;
+    }
+    
+    return b;
 
-        int n = s.length();
-        std::vector<int> dp(n + 1, 0);
-        dp[0] = 1;
-        dp[1] = 1;
-
-        for (int i = 2; i <= n; ++i) {
-            int oneDigit = s[i - 1] - '0';
-            int twoDigits = std::stoi(s.substr(i - 2, 2));
-
-            if (oneDigit != 0) {
-                dp[i] += dp[i - 1];
-            }
-
-            if (10 <= twoDigits && twoDigits <= 26) {
-                dp[i] += dp[i - 2];
-            }
-        }
-
-        return dp[n];
     }
 };
