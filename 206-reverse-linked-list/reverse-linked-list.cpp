6f5be92304ae65c81d0634edac2d2c4431166a72
PRANJAL <pranjalrana474@gmail.com>
2024-06-11 12:53:07 +0530

    Time: 4 ms (55.34%) | Memory: 11.7 MB (34.72%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -1,45 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
-    // private:
-    // void reverse(ListNode* &head,ListNode* &curr,ListNode* prev){
-    //     if(curr == NULL){
-    //         head = prev;
-    //         return;
-    //     }
-    //     ListNode* forward = curr->next;
-    //     reverse(head, forward, curr);
-    //     curr->next = prev;
-    // }
 public:
-
-    // ListNode* reverse(ListNode* head){
-    //     if(head -> next == NULL){
-    //         return head;
-    //     }
-
-    //     ListNode* newhead = reverse(head -> next);
-    //     head-> next-> next = head;
-    //     head -> next = NULL;
-    //     return newhead;
-    // }
-    // ListNode* reverseList(ListNode* head) {
-    //     if(head == NULL)return NULL;
-    //    return reverse(head);
-    // }
-    ListNode* reverseList(ListNode* head){
+    ListNode* reverseList(ListNode* head) {
         if(head == NULL || head -> next == NULL)return head;
-        ListNode* prev = NULL;
+
         ListNode* curr = head;
-        ListNode* n = head -> next;
+        ListNode* prev = NULL;
+        ListNode* next;
 
         while(curr != NULL){
+            next = curr -> next;
             curr -> next = prev;
             prev = curr;
-            curr = n;
-            if(n !=NULL){
-                n = n -> next;
-            }
+            curr = next;
         }
+
         return prev;
     }
 };
-
