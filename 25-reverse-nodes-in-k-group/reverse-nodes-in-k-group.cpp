93028ab1af60dc7a345c3df21e76e049b6693a3a
PRANJAL <pranjalrana474@gmail.com>
2024-06-11 16:10:57 +0530

    Time: 9 ms (63.36%) | Memory: 15.1 MB (38.13%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -1,45 +1,35 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-ListNode* Kreverse(ListNode* head,int k,int n){
-
-        if(n<k)return head;
-        ListNode* next=NULL;
-        ListNode* curr=head;
-        ListNode* prev=NULL;
-        int cnt=0;
-        while(cnt<k and curr!=NULL){
-            next=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=next;
-            cnt++;
-        }
-        if(next!=NULL)head->next=Kreverse(next,k,n-k);
-
-        return prev;
-    }
+ListNode* kReverse(ListNode* head,int k, int n){
+    if(n < k)return head;
     
+    ListNode* curr = head;
+    ListNode* next1 = NULL;
+    ListNode* prev = NULL;
+
+    int cnt = 0;
+    while(cnt <k && curr != NULL){
+        next1 = curr -> next;
+        curr -> next = prev;
+        prev = curr;
+        curr = next1;
+        cnt++;
+    }
+    if(next1 != NULL)head -> next = kReverse(next1, k, n-k);
+    return prev;
+
+}
     ListNode* reverseKGroup(ListNode* head, int k) {
         if(head == NULL){
             return NULL;
         }
-
-         ListNode* temp=head;
-        int n=0;
+        int n =0;
+        ListNode* temp = head;
         while(temp){
-            temp=temp->next;
+            temp = temp -> next;
             n++;
         }
-        return Kreverse(head,k,n);
+
+        return kReverse(head, k, n);
     }
 };
