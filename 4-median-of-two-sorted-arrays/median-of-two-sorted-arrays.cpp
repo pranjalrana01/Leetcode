c69a20d311476e2d8325f152201dc5f7bbfe83ab
PRANJAL <pranjalrana474@gmail.com>
2024-06-26 14:52:06 +0530

    Time: 16 ms (91.46%) | Memory: 94.5 MB (73.07%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if(nums2.size() < nums1.size())return findMedianSortedArrays(nums2, nums1);
         int n1 = nums1.size();
         int n2 = nums2.size();
 
-        int low = 0, high = n1;
-        while(low <= high){
-            int cut1 = (low + high)/2;
-            int cut2 = (n1+n2+1)/2-cut1;
+        if(n1 > n2)return findMedianSortedArrays(nums2, nums1);
 
-            int left1 = (cut1 == 0)?INT_MIN : nums1[cut1-1];
-            int left2 = (cut2 == 0)?INT_MIN : nums2[cut2-1];
+        int low = 0;
+        int high = n1;
+        int left =  (n1+ n2+ 1)/2;
+        int n = n1+n2;
 
-            int right1 = (cut1 == n1)?INT_MAX : nums1[cut1];
-            int right2 = (cut2 == n2)?INT_MAX : nums2[cut2];
+        while(low<=high){
+            int mid1 = (low+high) >> 1;
+            int mid2 = left - mid1;
+            int l1 = (mid1-1 >= 0)?nums1[mid1-1] : INT_MIN;
+            int l2 = (mid2-1 >= 0)? nums2[mid2-1] : INT_MIN;
 
-            if(left1 <= right2 && left2 <= right1){
-                if((n1+n2) %2 ==0) return (max(left1, left2) + min(right1, right2))/2.0;
+            int r1 = (mid1 < n1)? nums1[mid1] : INT_MAX;
+            int r2 = (mid2 < n2)? nums2[mid2] : INT_MAX;
 
-                else
-                return max(left1, left2);
+            if(l1 <= r2 && l2 <= r1){
+                if(n%2 == 1)return max(l1, l2);
+                return (double)(max(l1, l2)+min(r1, r2))/ 2.0;
             }
-
-            else if(left1 > right2){
-                high = cut1-1;
+            else if(l1>r2){
+                high = mid1-1;
             }
-            else{
-                low = cut1 + 1;
-            }
+            else low = mid1+1;
         }
         return 0.0;
     }
