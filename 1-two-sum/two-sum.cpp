d8561e456515258454547a068f60de326ed4a40e
PRANJAL <pranjalrana474@gmail.com>
2024-07-11 12:02:19 +0530

    Time: 12 ms (59.15%) | Memory: 14.2 MB (18.37%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,17 +1,17 @@
-#include<bits/stdc++.h>
 class Solution {
 public:
-     vector<int> twoSum(vector<int> &nums, int target) {
- unordered_map<int, int> visited;
-        int len = nums.size();
-        for (int i = 0; i < len; ++i) {
-            int n = nums[i];
-            int complement = target - n;
-            if (visited.count(complement)) {
-                return {visited[complement], i};
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int>ans;
+        unordered_map<int, int>mpp;
+        for(int i=0; i<nums.size(); i++){
+            int need = target - nums[i];
+            if(mpp.find(need) !=0){
+                ans.push_back(i);
+                ans.push_back(mpp[need]);
+                break;
             }
-            visited[n] = i;  // assume that each input would have exactly one solution
+            mpp[nums[i]] = i;
         }
-        return {};
+        return ans;
     }
-}; 
+};
